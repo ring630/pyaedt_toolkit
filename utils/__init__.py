2d10feb3034e7fa01b7c488afaa9e19d8e80eaa7
hzhou <ring630@gmail.com>
2022-11-03 09:03:09 +0100

    minor change

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,9 @@
 import time
+import os
+import tempfile
+import shutil
+from pyaedt import generate_unique_name, examples
+
 
 def str2float(des_type, val):
     if isinstance(val, float):
@@ -30,3 +35,17 @@ def str2float(des_type, val):
 
 def log_info(name):
     print(time.ctime(), name)
+
+
+def get_galileo_exmaple_board():
+    tmpfold = tempfile.gettempdir()
+    temp_folder = os.path.join(tmpfold, generate_unique_name("Example"))
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+    example_path = examples.download_aedb()
+    targetfolder = os.path.join(temp_folder, "Galileo.aedb")
+    if os.path.exists(targetfolder):
+        shutil.rmtree(targetfolder)
+    shutil.copytree(example_path[:-8], targetfolder)
+    targetfile = os.path.join(targetfolder)
+    return targetfile
