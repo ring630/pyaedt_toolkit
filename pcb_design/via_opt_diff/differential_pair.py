46bb7d4f6ef461b07b6827c868dae025544451c4
Hui <ring630@gmail.com>
2022-12-22 14:12:22 +0100

    f

--- pcb_design/via_opt_diff/differential_pair.py
+++ pcb_design/via_opt_diff/differential_pair.py
@@ -17,8 +17,6 @@ class DiffPairType1(Bench3DL):
         self.appedb.add_design_variable("$sig_gap_1", "0.1mm")
         self.appedb.add_design_variable("$via_distance", "0.8mm")
 
-
-
         # Place pin/via
         data = {
             0: ["GND", "SIG_P", "SIG_N", "GND"]
@@ -26,7 +24,16 @@ class DiffPairType1(Bench3DL):
         self.gnd_planes = self.init_design(self.layer_count)
         self.gnd_planes[self.sig_layers[0]].delete()
         self.gnd_planes[self.sig_layers[1]].delete()
+        for name, val in self.appedb.stackup.layers.items():
+            if name in self.sig_layers:
+                val.color = (255, 255, 0)
+                val.transparency = 20
+            else:
+                val.color = (192, 192, 192)
+                val.transparency = 90
 
+        sig_p_0, sig_path_p_0, sig_n_0, sig_path_n_0 = None, None, None, None
+        sig_p_1, sig_path_p_1, sig_n_1, sig_path_n_1 = None, None, None, None
         for x, d in data.items():
             for y, net_name in enumerate(d):
                 via_x_loc = "$drill_offset_x+{}*$via_distance".format(x)
@@ -34,14 +41,12 @@ class DiffPairType1(Bench3DL):
                 x_loc = "{}*$via_distance".format(x)
                 y_loc = "{}*$via_distance".format(y)
 
-
-
                 if net_name.endswith("_P"):
                     x_loc_2 = "{}-{}".format(x_loc, "($via_distance-$sig_gap_0-$sig_width_0)/2")
                     y_loc_2 = "{}+{}".format(y_loc, "($via_distance-$sig_gap_0-$sig_width_0)/2")
-                    sig_path = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_n, y_loc_2)]
+                    sig_path_p_0 = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_n, y_loc_2)]
                     sig_p_0 = self.appedb.core_primitives.create_trace(
-                        sig_path,
+                        sig_path_p_0,
                         layer_name=self.sig_layers[0],
                         width="$sig_width_0",
                         net_name=net_name,
@@ -49,9 +54,9 @@ class DiffPairType1(Bench3DL):
                     )
                     x_loc_2 = "{}+{}".format(x_loc, "($via_distance-$sig_gap_1-$sig_width_1)/2")
                     y_loc_2 = "{}+{}".format(y_loc, "($via_distance-$sig_gap_1-$sig_width_1)/2")
-                    sig_path = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_p, y_loc_2)]
+                    sig_path_p_1 = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_p, y_loc_2)]
                     sig_p_1 = self.appedb.core_primitives.create_trace(
-                        sig_path,
+                        sig_path_p_1,
                         layer_name=self.sig_layers[1],
                         width="$sig_width_1",
                         net_name=net_name,
@@ -60,9 +65,9 @@ class DiffPairType1(Bench3DL):
                 elif net_name.endswith("_N"):
                     x_loc_2 = "{}-{}".format(x_loc, "($via_distance-$sig_gap_0-$sig_width_0)/2")
                     y_loc_2 = "{}-{}".format(y_loc, "($via_distance-$sig_gap_0-$sig_width_0)/2")
-                    sig_path = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_n, y_loc_2)]
+                    sig_path_n_0 = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_n, y_loc_2)]
                     sig_n_0 = self.appedb.core_primitives.create_trace(
-                        sig_path,
+                        sig_path_n_0,
                         layer_name=self.sig_layers[0],
                         width="$sig_width_0",
                         net_name=net_name,
@@ -70,9 +75,9 @@ class DiffPairType1(Bench3DL):
                     )
                     x_loc_2 = "{}+{}".format(x_loc, "($via_distance-$sig_gap_1-$sig_width_1)/2")
                     y_loc_2 = "{}-{}".format(y_loc, "($via_distance-$sig_gap_1-$sig_width_1)/2")
-                    sig_path = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_p, y_loc_2)]
+                    sig_path_n_1 = [(x_loc, y_loc), (x_loc_2, y_loc_2), (self.x_limit_p, y_loc_2)]
                     sig_n_1 = self.appedb.core_primitives.create_trace(
-                        sig_path,
+                        sig_path_n_1,
                         layer_name=self.sig_layers[1],
                         width="$sig_width_1",
                         net_name=net_name,
@@ -80,15 +85,26 @@ class DiffPairType1(Bench3DL):
                     )
 
                 pos = [via_x_loc, via_y_loc]
-                self.appedb.core_padstack.place_padstack(pos, "via_200", net_name=net_name)
+                via = self.appedb.core_padstack.place_padstack(pos, "via_200", net_name=net_name)
+                if not net_name == "GND":
+                    layer = self.sig_layers[-1]
+                    via.set_backdrill_bottom(layer, 0.5e-3)
 
-        #self.appedb.core_hfss.create_wave_port(sig_p.id, sig_path[-1], "wave_port", 50, 10, 10)
+        # self.appedb.core_hfss.create_wave_port(sig_p.id, sig_path[-1], "wave_port", 50, 10, 10)
+        self.appedb.core_hfss.create_differential_wave_port(
+            sig_p_0.id, sig_path_p_0[-1], sig_n_0.id, sig_path_n_0[-1], "port_0", 8, 8
+        )
+        self.appedb.core_hfss.create_differential_wave_port(
+            sig_p_1.id, sig_path_p_1[-1], sig_n_1.id, sig_path_n_1[-1], "port_1", 8, 3
+        )
 
-        #self.appedb.core_nets.plot(None)
+        setup = self.appedb.create_hfss_setup("setup1")
+
+        # self.appedb.core_nets.plot(None)
         self.save_edb_and_close(self.name)
 
 
-
 if __name__ == '__main__':
-    DiffPairType1(layer_count=8,
-                   work_dir=r"C:\ansysdev\_aedt_workspace\pyaedt_pcb_design").run()
+    DiffPairType1(layer_count=16,
+                  sig_layers=("TOP", "L6"),
+                  work_dir=r"C:\ansysdev\_aedt_workspace\pyaedt_pcb_design").run()
